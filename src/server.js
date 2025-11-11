205d2c9451e326aa10acf9befe3b31fbb0ec7fd7
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-10 21:47:17 -0500

    feat: estructura inicial del proyecto

---
+++ src/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const routes = require('./routes/index');
+
+const app = express();
+const cookieParser = require("cookie-parser");
+
+// Middleware
+app.use(cors({
+ origin: "http://localhost:5173", // 👈 tu frontend
+ credentials: true, // 👈 necesario para cookies
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//*** ROUTE FOR WELCOME 
+app.get('/', (req, res) => {
+ res.json({ message: 'Bienvenido al backend de fp-003!!' });
+});
+
+//*** ROUTES
+/*
+app.use('/api/auth', routes.authRoutes);
+app.use('/api/menu', routes.menuRoutes);
+app.use('/api/catalog', routes.catalogRoutes);
+app.use('/api/person', routes.personRoutes);
+app.use('/api/entrepreneurship', routes.entrepreneurshipRoutes);
+app.use('/api/career', routes.careerRoutes);
+app.use('/api/event', routes.eventRoutes);
+*/
+
+//*** GLOBAL ERROR HANDLER (opcional, pero recomendado para producción)
+//*** Manejador de errores global simple (opcional, pero recomendado para producción)
+app.use((err, req, res, next) => {
+ console.error(err.stack);
+ res.status(500).send('¡Algo salió mal en el servidor!');
+})
+
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+ console.log(`fp-003: Server is running on port ${PORT}`);
+});
