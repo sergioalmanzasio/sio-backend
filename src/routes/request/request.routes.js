64b38db41df01da759bc02209c4e1f7bf3babf1a
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-25 17:03:30 -0500

    refactor: nuevas rutas

--- src/routes/request/request.routes.js
+++ src/routes/request/request.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { addServiceRequest, validatePendingRequest } from '../../controllers/request/request.controller.js';
+import { addServiceRequest, validatePendingRequest, getServiceRequestByClient, getServiceRequestDetails } from '../../controllers/request/request.controller.js';
 
 router.post('/add', addServiceRequest);
 router.post('/validate-pending-request', validatePendingRequest);
+router.post('/client', getServiceRequestByClient);
+router.post('/details', getServiceRequestDetails);
 
 export default router;
