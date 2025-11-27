a6cb7ba4f48a7b7779ef92f2aa416d9f3bf559ab
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-27 17:15:11 -0500

    refactor: implementación de nueva ruta

--- src/routes/signup/signup.routes.js
+++ src/routes/signup/signup.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { signUp, signUpGenerateCode, signUpVerifyCode } from '../../controllers/signup/signup.controller.js';
+import { signUp, signUpGenerateCode, signUpVerifyCode, registerInternalUser } from '../../controllers/signup/signup.controller.js';
 
 router.post('/', signUp);
 router.post('/generate-code', signUpGenerateCode);
 router.post('/verify-code', signUpVerifyCode);
+router.post('/register-internal-user', registerInternalUser);
 
 export default router;
