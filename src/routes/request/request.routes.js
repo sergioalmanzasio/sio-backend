b0e2dabe126311cede4048b0603c3b447ae44b39
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-27 17:14:51 -0500

    refactor: implementación de nuevas rutas

--- src/routes/request/request.routes.js
+++ src/routes/request/request.routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { addServiceRequest, validatePendingRequest, getServiceRequestByClient, getServiceRequestDetails } from '../../controllers/request/request.controller.js';
+import { addServiceRequest, validatePendingRequest, getServiceRequestByClient, getServiceRequestDetails, 
+ cancelServiceRequestByClient, getServiceRequestByServiceCoordinator } from '../../controllers/request/request.controller.js';
 
 router.post('/add', addServiceRequest);
 router.post('/validate-pending-request', validatePendingRequest);
 router.post('/client', getServiceRequestByClient);
 router.post('/details', getServiceRequestDetails);
+router.post('/cancel-by-client', cancelServiceRequestByClient);
+router.post('/service-coordinator', getServiceRequestByServiceCoordinator);
 
 export default router;
