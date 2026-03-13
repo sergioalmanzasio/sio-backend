c04d9396e3c8871a1aa241b1aef47103d4f95c2d
Ricardo Velasquez <rianveal@gmail.com>
2026-03-13 15:18:45 -0500

    feat: nuevas rutas

--- src/routes/admin/bonus.routes.js
+++ src/routes/admin/bonus.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { createBonus, updateBonus, getActiveBonus, getBonusHistory } from '../../controllers/admin/bonus.controller.js';
+import { createBonus, updateBonus, getActiveBonus, getBonusHistory, getBonusesRequestedPayment, paidBonuses } from '../../controllers/admin/bonus.controller.js';
 
 router.post('/', createBonus);
 router.put('/:id', updateBonus);
 router.get('/active', getActiveBonus);
 router.get('/history', getBonusHistory);
+router.get('/requested-payment', getBonusesRequestedPayment);
+router.post('/paid', paidBonuses);
 
 export default router;
