32c0ee5760578eea7f2979fb5871eea2303df35e
Ricardo Velasquez <rianveal@gmail.com>
2026-03-15 00:07:26 -0500

    refactor: ajustes varios para signup.

--- src/controllers/signup/signup.controller.js
+++ src/controllers/signup/signup.controller.js
@@ -224,14 +224,13 @@ export const signUpGenerateCode = async (req, res) => {
     });
   }
 
-
   const code = generateVerificationCode();
   const expiresAt = getExpirationDate();
 
   // Save code in database
   pool.query(
-    "INSERT INTO signup_code (email, code, expires_at) VALUES ($1, $2, $3)",
-    [email, code, expiresAt],
+    "INSERT INTO signup_code (email, code) VALUES ($1, $2)",
+    [email, code],
     async (err, result) => {
       if (err) {
         return res
@@ -271,37 +270,33 @@ export const signUpVerifyCode = async (req, res) => {
   }
 
   // Verify code
-  pool.query(
+  const validateCodeAndExpires = await pool.query(
     "SELECT * FROM signup_code WHERE email = $1 AND code = $2 AND expires_at > NOW()",
-    [email, code],
-    (err, result) => {
-      if (err) {
-        return res
-          .status(500)
-          .json({ message: "Error al validar código." });
-      }
-      if (result.rows.length === 0) {
-        return res
-          .status(400)
-          .json({ message: "Código inválido." });
-      }
-      // Update is_used to true
-      pool.query(
-        "UPDATE signup_code SET is_used = $1 WHERE email = $2 AND code = $3 AND expires_at > NOW()",
-        [true, email, code],
-        (err, result) => {
-          if (err) {
-            return res
-              .status(500)
-              .json({ message: "Error al validar código." });
-          }
-          return res
-            .status(200)
-            .json({ message: "Código verificado exitosamente." });
-        }
-      );
-    }
+    [email, code]
   );
+
+  if (validateCodeAndExpires.rows.length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Código inválido." });
+  }
+
+  // Update is_used to true
+  const updateCodeAndExpires = await pool.query(
+    "UPDATE signup_code SET is_used = $1 WHERE email = $2 AND code = $3 AND expires_at > NOW() RETURNING *",
+    [true, email, code]
+  );
+
+  if (updateCodeAndExpires.rows.length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Código inválido." });
+  }
+
+  return res
+    .status(200)
+    .json({ message: "Código verificado exitosamente." });
+
 };
 
 // Register internal user
@@ -392,4 +387,76 @@ export const registerInternalUser = async (req, res) => {
       }
     );
   });
-}    
+}
+
+export const registerInternalUserWithoutToken = async (req, res) => {
+  const { document, document_type_id, name, middle_name, last_name, email, phone, role_id, password } = req.body;
+  if (!document || !document_type_id || !name || !last_name || !email || !phone || !role_id || !password) {
+    return res
+      .status(400)
+      .json({ message: "Todos los campos son obligatorios." });
+  }
+  const hash = await hashPassword(password);
+
+  validatePersonExistByDocumentEmailPhone(document, email, phone)
+    .then((validatePersonExist) => {
+      if (validatePersonExist.process === "error") {
+        return res.status(400).json({ message: validatePersonExist.message });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ message: "Error al validar persona." });
+    })
+
+  validateUserExist(email)
+    .then((validateUserExist) => {
+      if (validateUserExist.process === "error") {
+        return res.status(400).json({ message: validateUserExist.message });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).json({ message: "Error al validar usuario." });
+    })
+
+  const username = email;
+  const isActive = true;
+  const created_by = transversalUUID();
+  const updated_by = transversalUUID();
+
+  // insert person
+  pool.query(
+    "INSERT INTO persons (document, document_type_id, name, middle_name, last_name, email, phone, created_by, updated_by) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+    [document, document_type_id, name, middle_name, last_name, email, phone, created_by, updated_by],
+    async (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: "Error al crear persona." });
+      }
+      const person_id = result.rows[0].id;
+      // insert user
+      pool.query(
+        "INSERT INTO users (person_id, username, password, is_active, created_by, updated_by) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+        [person_id, username, hash, isActive, created_by, updated_by],
+        async (err, result) => {
+          if (err) {
+            return res.status(500).json({ message: "Error al crear usuario." });
+          }
+          const user_id = result.rows[0].id;
+          // insert user_role
+          pool.query(
+            "INSERT INTO user_roles (user_id, role_id, created_by, updated_by) VALUES ($1, $2, $3, $4)",
+            [user_id, role_id, created_by, updated_by],
+            async (err, result) => {
+              if (err) {
+                return res.status(500).json({ message: "Error al crear rol." });
+              }
+              return res.status(200).json({ message: "Usuario creado exitosamente." });
+            }
+          );
+        }
+      );
+    }
+  );
+
+} 
