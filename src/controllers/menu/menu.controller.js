ec0d95b9518d557818d302da3a3fb19b69d53afb
Ricardo Velasquez <rianveal@gmail.com>
2026-03-21 16:57:43 -0500

    refactor: manejo de seguro de rutas (MenuUsers)

--- src/controllers/menu/menu.controller.js
+++ src/controllers/menu/menu.controller.js
@@ -1,6 +1,4 @@
 import pool from "../../config/db.config.js";
-import authConfig from "../../config/auth.config.js";
-import jwt from "jsonwebtoken";
 
 // Get option menu by role id
 export const getOptionMenuByRoleId = (req, res) => {
@@ -10,41 +8,26 @@ export const getOptionMenuByRoleId = (req, res) => {
       .status(400)
       .json({ message: "Debe proporcionar un ID de rol válido." });
   }
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({
-      process: "session-expired",
-      message:
-        "Por seguridad, tu sesión ha caducado. Accede nuevamente a SIO para seguir navegando.",
-    });
-  }
-  jwt.verify(token, authConfig.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({
-        process: "session-expired",
-        message:
-          "No pudimos validar tu sesión. Accede nuevamente a SIO para continuar con seguridad.",
+
+  pool.query(
+    `SELECT op."name", op.url 
+            FROM role_options_app roa 
+            JOIN roles rl ON roa.role_id = rl.id 
+            JOIN options_app op ON op.id = roa.option_id 
+            WHERE roa.role_id = $1 AND roa.is_active = TRUE order by op.order_number`,
+    [role_id],
+    (err, result) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "Error al consultar opciones del menu." });
+      }
+      return res.status(200).json({
+        message: "Opciones del menu encontradas.",
+        data: result.rows,
+        count: result.rowCount,
       });
     }
-    pool.query(
-      `SELECT op."name", op.url 
-              FROM role_options_app roa 
-              JOIN roles rl ON roa.role_id = rl.id 
-              JOIN options_app op ON op.id = roa.option_id 
-              WHERE roa.role_id = $1 AND roa.is_active = TRUE order by op.order_number`,
-      [role_id],
-      (err, result) => {
-        if (err) {
-          return res
-            .status(500)
-            .json({ message: "Error al consultar opciones del menu." });
-        }
-        return res.status(200).json({
-          message: "Opciones del menu encontradas.",
-          data: result.rows,
-          count: result.rowCount,
-        });
-      }
-    );
-  });
+  );
+
 };
--- src/routes/menu/menu.routes.js
+++ src/routes/menu/menu.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import { getOptionMenuByRoleId } from '../../controllers/menu/menu.controller.js';
+import { authMiddleware } from '../../middlewares/authMiddleware.js';
 
-router.get('/options/:role_id', getOptionMenuByRoleId);
+router.get('/options/:role_id', authMiddleware, getOptionMenuByRoleId);
 
 export default router;
