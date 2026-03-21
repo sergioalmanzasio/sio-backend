813eb77e01ed477d3d105cc3f566d3129089b15d
Ricardo Velasquez <rianveal@gmail.com>
2026-03-21 16:52:18 -0500

    refactor: manejo de seguro de rutas (ServiceRequest-Admin)

--- src/routes/admin/admin.routes.js
+++ src/routes/admin/admin.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import { getAllServiceRequests } from '../../controllers/admin/admin.controller.js';
+import { authMiddleware } from '../../middlewares/authMiddleware.js';
 
-router.get('/service-requests', getAllServiceRequests);
+router.get('/service-requests', authMiddleware, getAllServiceRequests);
 
 export default router;
